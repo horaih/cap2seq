24a310ebe024392a5a01394f73b1b8b45cd2deaa
horaih <hiroyasu.horai@gmail.com>
2023-03-19 12:40:54 +0900

    add pickup_imsi for picking the IMSI used in the capture up.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ struct Args {
     pcapfile: String,
     #[arg(short = 'p', long, value_name = "PORT NUMBER(s)")]
     http2ports: Option<String>,
+    #[arg(short = 'i', long, value_name = "IMSI")]
+    imsi: Option<String>,
 }
 
 fn main() {
@@ -43,6 +45,13 @@ fn main() {
         Ok(rtshark) => rtshark,
     };
 
+    if args.imsi == None {
+        pickup_imsi(&mut rtshark);
+        return;
+    }
+
+    let _imsi = &args.imsi.unwrap();
+
     // read packets until the end of the PCAP file
     while let Some(packet) = rtshark.read().unwrap_or_else(|e| {
         eprintln!("Error parsing TShark output: {e}");
@@ -56,4 +65,64 @@ fn main() {
             }
         }
     }
+}
+
+fn pickup_imsi(rtshark: &mut rtshark::RTShark) {
+    const IMSI_PREFIX: usize = 5; // imsi-
+    const IMSI_LENGTH: usize = 13; // 2089300007487
+
+    let mut imsi_list: Vec<String> = vec![];
+
+    // read packets until the end of the PCAP file
+    while let Some(packet) = rtshark.read().unwrap_or_else(|e| {
+        eprintln!("Error parsing TShark output: {e}");
+        None
+    }) {
+        'packet: for layer in packet {
+            for metadata in layer {
+                let head = metadata.value().find("imsi-");
+                if head == None {
+                    continue;
+                }
+                let n = head.unwrap();
+                let tmp = substr(
+                    metadata.value(),
+                    n + IMSI_PREFIX,
+                    n + IMSI_PREFIX + IMSI_LENGTH,
+                );
+                let mut found_new_imsi = true;
+                for imsi in &imsi_list {
+                    if imsi != &tmp {
+                        found_new_imsi = true;
+                        break;
+                    }
+                    found_new_imsi = false;
+                }
+                if found_new_imsi {
+                    imsi_list.push(tmp.to_string());
+                    break 'packet;
+                }
+            }
+        }
+    }
+    let imsi_len = imsi_list.len();
+    if imsi_len == 0 {
+        println!("imsi not found in the capture file.");
+        return;
+    }
+    println!("following imsi(s) found.");
+    for imsi in imsi_list {
+        println!("{}", imsi);
+    }
+    return;
+}
+
+fn substr(line: &str, begin: usize, end: usize) -> String {
+    let mut tmp = "".to_string();
+    for (i, c) in line.chars().enumerate() {
+        if i >= begin && i < end {
+            tmp.push(c);
+        }
+    }
+    tmp
 }
